1320e1f31a209849a46fa80b499d9543459fabc3
Alexandr Kostrov <090890kan@gmail.com>
2018-07-20 13:51:26 +0300

    deleting wrong solution

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -5,7 +5,30 @@
  * @param {Function} Main - класс который должен стать наследником
  * @param {Function} Parent - родительский класс
  */
-function extendClass(Main, Parent) {
-    // здесь ваш код наследования
+function extendClass(Main, Parent) { 
+    Main.prototype = Object.create(Parent.prototype);
+    Main.prototype.constructor = Main;
 }
+
+function Animal() {
+}
+
+Animal.prototype.walk = function () {}
+
+
+function Rabbit() {
+}
+
+extendClass(Rabbit, Animal);
+
+Rabbit.prototype.jump = function () {}
+
+let rabbit = new Rabbit();
+
+console.log(rabbit instanceof Rabbit);
+console.log(rabbit instanceof Animal);
+
+console.log(typeof rabbit.walk === 'function');
+console.log(typeof rabbit.jump === 'function');
+
 
